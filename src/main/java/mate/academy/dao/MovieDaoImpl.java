489582011fa575864ccafa884b8ae4135a88c01d
alex <ursusandwolf@protonmail.com>
2026-02-28 16:40:35 +0200

    movie dao imp

--- src/main/java/mate/academy/dao/MovieDaoImpl.java
+++ src/main/java/mate/academy/dao/MovieDaoImpl.java
@@ -2,15 +2,36 @@ package mate.academy.dao;
 
 import java.util.Optional;
 import mate.academy.model.Movie;
+import org.hibernate.Session;
+
+import static mate.academy.util.HibernateUtil.getSessionFactory;
 
 public class MovieDaoImpl implements MovieDao {
     @Override
     public Movie add(Movie movie) {
-        return null;
+        try(Session session = getSessionFactory()
+                .getCurrentSession();
+        ) {
+            session.save(movie);
+            session.commit(movie);
+            return movie;
+        }
+        catch (Exception e) {
+            throw new DataProcessingException("Can't insert movie " + movie, e);
+        }
     }
 
     @Override
     public Optional<Movie> get(Long id) {
-        return Optional.empty();
+        try(Session session = getSessionFactory()
+                .getCurrentSession();
+        ) {
+            Object o = session.get(String.valueOf(id), Movie.class);
+            Movie res = (Movie) o;
+            return Optional.ofNullable(res);
+        }
+        catch (Exception e) {
+            throw new DataProcessingException("Can't find id " + id, e);
+        }
     }
 }
--- src/main/java/mate/academy/model/Movie.java
+++ src/main/java/mate/academy/model/Movie.java
@@ -1,8 +1,70 @@
 package mate.academy.model;
 
+import java.util.Objects;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "movies")
 public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column
     private String title;
+    @Column
     private String description;
 
+    public Movie() {
+
+    }
+
+    public Movie(String title) {
+        this.title = title;
+    }
+
+    public Movie(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Movie movie = (Movie) o;
+        return Objects.equals(getId(), movie.getId())
+                && Objects.equals(getTitle(), movie.getTitle())
+                && Objects.equals(getDescription(), movie.getDescription());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getTitle(), getDescription());
+    }
 }
